3ed041b896af937b57bef190f267ba49c9fe7378
Vladislav Balachevskyi <vbalachevs@e1r7p8.unit.ua>
2020-06-26 17:59:13 +0300

    fix main.c

--- client/src/main.c
+++ client/src/main.c
@@ -1,23 +1,11 @@
+  
 #include "client.h"
 #include "mxinet.h"
 #include "defines.h"
-#include "libmx.h"
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <pthread.h>
 
 int main(int argc, char *argv[]) {
     t_info *info = 0;
     gtk_init(&argc, &argv);
     mx_init(&info);
-
-    // system("leaks client");
-    // mx_show_window(info);
+    mx_show_window(info);
 }
